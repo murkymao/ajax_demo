5b2e664925284b5303855a985da13e6ed21f3000
murkymao <glaciefer.1@gmail.com>
2017-12-15 12:51:21 +0200

    add structure and main files

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>AJAX</title>
+</head>
+<body>
+<input id="shop_ip" type="button" value="Show my IP">
+<div id="myip"></div>
+
+<script src="script.js"></script>
+</body>
+</html>
--- ip.php
+++ ip.php
@@ -5,3 +5,5 @@
  * Date: 15.12.2017
  * Time: 12:40
  */
+
+	echo $_SERVER['REMOTE_ADDR'];
